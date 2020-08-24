a384ec97f65e901a1f03348bef78a37c98186efd
Tom Hendra <tomhendra@TomHs-MacBook-Pro.local>
2020-08-24 19:00:26 +0100

    added write functionality for adding new items

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { Box, Typography, Button } from "@material-ui/core";
+import { Box, Typography, Button, TextField } from "@material-ui/core";
 import ImageUpload from "./components/ImageUpload";
-import { useQuery, NetworkStatus } from "@apollo/client";
-import { ITEMS } from "./graphQl";
+import {
+  useQuery,
+  NetworkStatus,
+  useMutation,
+  useApolloClient,
+} from "@apollo/client";
+import { ITEMS, ADD_ITEM, ADD_ITEM_FRAGMENT } from "./graphQl";
 
 const App = () => {
-  const { error, data, refetch, networkStatus } = useQuery(ITEMS, {
+  const { error, data, networkStatus } = useQuery(ITEMS, {
     notifyOnNetworkStatusChange: true,
   });
+  const [addItem, { data: result }] = useMutation(ADD_ITEM);
+  const [title, setTitle] = useState("");
+  const [id, setId] = useState("");
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setTitle(event.target.value);
+  };
+
+  const handleAddItem = (event: React.FormEvent) => {
+    event.preventDefault();
+    addItem({ variables: { title: title } });
+    setTitle("");
+  };
 
   return (
     <Box className="App">
@@ -17,13 +36,23 @@ const App = () => {
           {networkStatus === NetworkStatus.refetch && "Refetching!"}
           {networkStatus === NetworkStatus.loading && "loading..."}
           {error && `Error! ${error.message}`}
-          {data?.Items.map(({ id, Title }: { id: number; Title: string }) => (
-            <Typography>{`${id} ${Title}`}</Typography>
+          {data?.Items.map(({ id, title }: { id: number; title: string }) => (
+            <Typography>{`${id} ${title}`}</Typography>
           ))}
         </Typography>
-        <Button variant="contained" onClick={() => refetch()}>
-          Refetch!
-        </Button>
+        <form noValidate autoComplete="off" onSubmit={handleAddItem}>
+          <TextField
+            value={title}
+            variant="outlined"
+            id="title-input"
+            label="Title"
+            onChange={handleTitleChange}
+            color="primary"
+          />
+          <Button variant="contained" type="submit">
+            Add Item
+          </Button>
+        </form>
       </header>
       <ImageUpload label="Drag and drop files to upload them to CLOUDINARY" />
     </Box>
