325649ec8f3107972e669ed12f88422d0fb3dd57
Tom Hendra <tomhendra@TomHs-MacBook-Pro.local>
2020-10-02 16:39:52 +0100

    removed unese imports

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import {
   CardMedia,
   CardActionArea,
 } from "@material-ui/core";
-import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import { useQuery, NetworkStatus } from "@apollo/client";
 import { ITEMS } from "./graphQl";
 import AddItemForm from "./components/AddItemForm";
